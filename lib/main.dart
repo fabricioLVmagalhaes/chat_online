9bb50c26d1ebe8cf831b0c694315502aa05c757d
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2019-07-13 17:57:58 -0300

    Balões de Mensagem

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,16 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
         body: Column(
           children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ChatMessage(),
+                  ChatMessage(),
+                  ChatMessage()
+                ],
+              ),
+            ),
+            Divider(height: 1.0,),
             Container(
               decoration: BoxDecoration(
                 color: Theme.of(context).cardColor,
@@ -115,3 +125,39 @@ class _TextComposerState extends State<TextComposer> {
     );
   }
 }
+
+class ChatMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage("https://abrilexame.files.wordpress.com/2019/05/grumpy-cat.jpg"),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Daniel",
+                  style: Theme.of(context).textTheme.subhead,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text("teste"),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
